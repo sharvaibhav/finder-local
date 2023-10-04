abc5250873be70861cca4f0af60193b378d92c05
Vaibhv Sharma <vaibhav.jiitu@gmail.com>
2023-10-05 00:22:28 +0200

    push finally working testing code

--- jest.config.js
+++ jest.config.js
@@ -1,13 +1,18 @@
-module.exports = {
-  preset: "ts-jest",
-  testEnvironment: "jsdom",
-  transform: {
-    "^.+\\.(js|jsx|ts|tsx)$": "babel-jest", // Adding Babel transformer
-  },
+const nextJest = require("next/jest");
+
+const createJestConfig = nextJest({
+  // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
+  dir: "./",
+});
+
+// Add any custom config to be passed to Jest
+const customJestConfig = {
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
+  testEnvironment: "jest-environment-jsdom",
   moduleNameMapper: {
     "^@/(.*)$": "<rootDir>/$1",
-    "\\.(css|less|scss|sass)$": "<rootDir>/__mocks__/styleMock.js", // Adjust the path
   },
-  testPathIgnorePatterns: ["/node_modules/", "/.next/"], // Ignoring node_modules and .next folder
-  transformIgnorePatterns: ["/node_modules/"], // This is essential to ensure that node_modules are not transformed
 };
+
+// createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
+module.exports = createJestConfig(customJestConfig);
